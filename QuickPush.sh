23f1c2a9a3604a044e341e031d577905895ed0aa
Space <Spacelessd@outlook.com>
2021-01-31 15:44:03 +0800

    First Commit

---
+++ QuickPush.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+git add --all
+git commit
+git push
+rm ./*.run
+rm ./*/*.run
+rm ./*/*/*.run
+rm ./*/*/*/*.run
+rm ./*/*/*/*/*.run
+rm ./*/*/*/*/*/*.run
+rm ./*.exe
+rm ./*/*.exe
+rm ./*/*/*.exe
+rm ./*/*/*/*.exe
+rm ./*/*/*/*/*.exe
+rm ./*/*/*/*/*/*.exe
